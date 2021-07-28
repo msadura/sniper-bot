e21b5e904c7c31e3d52b2122f7c06ed9b890c98d
Marek Sadura <m.sadura@outlook.com>
2021-07-28 09:23:04 +0200

    Add fanaside (FAN) token

--- constants.js
+++ constants.js
@@ -30,7 +30,7 @@ const SNIPE_TOKENS_CONFIG = {
 };
 
 // Put names of trigger tokens here or in env variable
-let SNIPE_TOKEN_NAMES = ['NETPLIX', 'PLYRS', 'LUCKYBDOGE'];
+let SNIPE_TOKEN_NAMES = ['NETPLIX', 'PLYRS', 'LUCKYBDOGE', 'FAN'];
 
 try {
   const snipeTokensEnv = process.env.SNIPE_TOKEN_NAMES.split(',').map(n => n.trim().toUpperCase());
