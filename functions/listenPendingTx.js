49f1dee0def0f63670914515cf8dd7f3a62270c2
Marek Sadura <m.sadura@outlook.com>
2021-07-30 19:42:41 +0200

    Loggin pending tx stats

--- functions/listenPendingTx.js
+++ functions/listenPendingTx.js
@@ -1,10 +1,22 @@
 const { getProvider } = require('../provider');
 
+// let good = 0;
+// let bad = 0;
+
 function listenPendingTx(callback) {
   console.log('🔫   -->   Listening pending tx');
   const provider = getProvider();
   provider.on('pending', async tx => {
     const transaction = await provider.getTransaction(tx);
+
+    // if (!transaction) {
+    //   bad++;
+    //   console.log('🔥', 'good tx', good, 'null tx', bad);
+    // } else {
+    //   good++;
+    //   console.log('🔫', 'good tx', good, 'null tx', bad);
+    // }
+
     callback && callback(transaction);
   });
 }
