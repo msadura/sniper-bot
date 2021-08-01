105bcd3ea02473b75e0c624ce751e3c7de4a6f97
Marek Sadura <m.sadura@outlook.com>
2021-08-01 13:19:35 +0200

    Master Doge Chef

--- constants.js
+++ constants.js
@@ -30,7 +30,7 @@ const SNIPE_TOKENS_CONFIG = {
 };
 
 // Put names of trigger tokens here or in env variable
-let SNIPE_TOKEN_NAMES = ['NETPLIX'];
+let SNIPE_TOKEN_NAMES = ['NETPLIX', 'MDCH'];
 
 try {
   const snipeTokensEnv = process.env.SNIPE_TOKEN_NAMES.split(',').map(n => n.trim().toUpperCase());
