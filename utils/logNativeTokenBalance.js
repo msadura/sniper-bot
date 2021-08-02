d906ae09c395f595d3823b6d8b7079955094d230
Marek Sadura <m.sadura@outlook.com>
2021-08-02 14:01:17 +0200

    try/catch load balance

--- utils/logNativeTokenBalance.js
+++ utils/logNativeTokenBalance.js
@@ -2,15 +2,31 @@ const { ethers } = require('ethers');
 const { NATIVE_TOKEN_SYMBOL } = require('../constants');
 const { getAccount } = require('../wallet');
 
-async function logNativeTokenBalance() {
-  const balance = await getAccount(true).getBalance();
+let retries = 0;
 
-  console.log(
-    '💰',
-    `Native token balance on main account: ${ethers.utils.formatEther(
-      balance
-    )} ${NATIVE_TOKEN_SYMBOL}`
-  );
+async function logNativeTokenBalance(retry = false) {
+  if (!retry) {
+    retries = 0;
+  }
+
+  try {
+    const balance = await getAccount(true).getBalance();
+
+    console.log(
+      '💰',
+      `Native token balance on main account: ${ethers.utils.formatEther(
+        balance
+      )} ${NATIVE_TOKEN_SYMBOL}`
+    );
+  } catch (e) {
+    if (retries < 5) {
+      retries = retries + 1;
+      logNativeTokenBalance(true);
+      return;
+    }
+
+    console.log('🔥', 'Failed to load account balance');
+  }
 }
 
 module.exports = logNativeTokenBalance;
