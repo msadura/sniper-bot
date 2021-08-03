0ea96dd027d47582a073c5982912a91fbc996878
Marek Sadura <m.sadura@outlook.com>
2021-08-03 12:09:25 +0200

    SNipe puppy

--- constants.js
+++ constants.js
@@ -30,7 +30,7 @@ const SNIPE_TOKENS_CONFIG = {
 };
 
 // Put names of trigger tokens here or in env variable
-let SNIPE_TOKEN_NAMES = ['NETPLIX', 'MDCH'];
+let SNIPE_TOKEN_NAMES = ['PUPPY', 'MDCH'];
 
 try {
   const snipeTokensEnv = process.env.SNIPE_TOKEN_NAMES.split(',').map(n => n.trim().toUpperCase());
